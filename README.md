5867944bb3462c974c237d5cb02c7492788000e1
Rafael <rafael@debian-BULLSEYE-live-builder-AMD64>
2022-02-05 14:36:45 -0300

    New version:
    - Improvements in managing library organization;
    - Better documentation;

---
+++ README.md
@@ -0,0 +1,184 @@
+# Nyaa.si Auto Downloader #
+
+A simple script to automate checking for new episodes of animes from your
+preferred uploaders at nyaa.si, adding them to transmission-daemon, and
+managing completed downloads.
+
+Requires transmission-daemon and transmission-remote, Python 3.5+
+
+Add it to your crontab or similar service to run automatically.
+
+Note that the script looks for config.json at the current working directory, so
+change to the directory that contains the config.json file before running the
+script. For example, if using cron to call the script, do a simple shell script
+with the following lines:
+
+~~~shell
+#!/bin/sh
+cd /path/to/config
+/path/to/getnyaa.py
+~~~
+
+If your config is in the same directory as the getnyaa script, then just add
+this as your command in your crontab:
+
+~~~shell
+cd /path/to/getnyaa_dir && ./getnyaa.py
+~~~
+
+The script prints to stdout what it is doing for each entry in the RSS that
+matches the search rules defined in config.json. To log to a file instead, just
+redirect. For example, the previous script could be rewritten to:
+
+~~~shell
+#!/bin/sh
+cd /path/to/config
+/path/to/getnyaa.py > /var/log/getnyaa.log
+~~~
+
+For example, to run the script to check for new episodes each hour and log the
+output, add this line to your crontab using `crontab -e`:
+
+`0 * * * * cd /path/to/getnyaa && ./getnyaa.py > ~/getnyaa.log`
+
+## How to configure ##
+
+The config.json is structured like this:
+
+- user: your remote transmission-daemon username;
+- password: your remote transmission-daemon password;
+- download_dir: path where transmision-daemon saves completed torrents;
+- library_dir: path to where your library is. It'll be used to check if a
+  episode already exists, and to copy new episodes.
+- sources: list of uploaders from nyaa.si;
+
+"sources" is a list of objects with the following keys:
+
+- user: username of the uploader, the last part of the user url, e.g.: if the
+  uploader list of torrents is in `https://nyaa.si/user/sff`, the the user is
+  "sff".
+- animes: list of objects that have configuration on how to search and organize
+  the anime.
+
+"animes" is a list of objects with the following keys:
+
+- name: The name of the anime. It'll be used as the directory name inside your
+  "library_dir";
+- episode_re: A regular expression that is matched against the torrent name.
+  Must have a capture group of the episode number;
+- season (optional): The season number. See EPISODE ORGANIZATION for details;
+- season_start (optional): See EPISODE ORGANIZATION for details;
+- seaosn_end (optional): See EPISODE ORGANIZATION for details;
+
+See the included "config.json" as an example.
+
+## Writing a __epsiode_re__ expression ##
+
+A simple way to create a __epsiode_re__ expression is to visit the torrent
+description, copying the torrent title, and doing the following edits:
+
+- Delete tags like `[HEVC]`, and anything thad doens't contribute to identify
+  the particular anime episode.
+- If there's both overall and seasonal episode numbers, delete one of them.
+  Leave the text right before the episode number so it can be used to identify
+  where the episode number is.
+- Replace other text between the anime name and the episode number with `.*`.
+- Replace the episode number with (\\d+). If you are interested on the overral
+  episode number, change the "Ep. 33" with "Ep. (\\d+)". If you are interested
+  in seasonal episode number, change the "S02E01" with "S02E(\\d+)".
+
+Example:
+
+Consider the following torrent title:
+
+`[Judas] Shingeki no Kyojin - S04E20 (Attack on Titan) (Ep.79) [1080p][HEVC x265 10bit][Multi-Subs] (Weekly)`
+
+There's both seasonal and overall episode numbers.
+
+For overall numbering, you can use the following __search_re__ :
+
+`Shingeki no Kyojin.*Ep.(\\d+)`
+
+For seasonal numbering, you can use the following __search_re__:
+
+`Shingeki no Kyojin.*S04E(\\d+)`
+
+Check the next section for more complex situations, like converting seasonal
+episode numbers to overall episode numbers.
+
+## EPISODE ORGANIZATION ##
+
+Consider __library_dir__ as your library root. For each anime configured in
+config.json, a path for seaching and copying episodes is constructed.
+
+In the following examples, note that the filename extension (".mkv") is just an
+example, the script doesn't care about it in practice.
+
+### USING OVERALL EPISODE NUMBERING ###
+
+If you prefer that all episodes are placed inside the anime folder, with
+episodes numbered from 1 to the last, without separating by seasons, then just
+don't use the __season__ key when configuring an anime.
+
+For example, the following anime configuration:
+
+`"name": "Attack on Titan",
+"search_re": "Shingeki no Kyojin.*Ep..(\\d+)"`
+
+Will check for and copy new episodes like this:
+
+`Attack on Titan/Attack on Titan - 33.mkv`
+
+If the uploader only uses seasonal numbering, you can convert it to overall
+numbering using __season_start__. For example, the first episode of the fourth
+season of Attack on Titan is the 60th episode in overall numbering. Adding
+`"season_start": 60` to the configuration, will convert episode 1 of season 4 to
+episode 60 overall:
+
+~~~json
+{
+  "name": "Attack on Titan",
+  "search_re": "Shingeki no Kyojin.*S04E(\\d+)",
+  "season_start": 60
+}
+~~~
+
+Will convert S04E01 to `Shingeki no Kyojin - 60.mkv`.
+
+### USING SEASONAL EPISODE NUMBERING ###
+
+If you prefer organized like some media servers do (e.g. Jellyfin), separated
+by seasons, use the __season__ key when configuring an anime like this:
+
+~~~json
+"name": "Attack on Titan",
+"search_re": "Shinkegi no Kyojin.*S04E(\\d+)",
+"season": 4
+~~~
+
+Then, the script will check for and add new episodes like this:
+
+`Attack on Titan/Season 04/Attack on Titan - S04E01.mkv`
+
+If the uploader doesn't name the torrent with a seasonal numbering, you can
+convert overall numbering to seasonal numbering using __season_start__ and,
+optionally, __season_end__. For example, with the following configuration:
+
+~~~json
+"name": "Attack on Titan",
+"search_re": "Shingeki no Kyokin.*Ep..(\\d+)",
+"season": 2,
+"season_start": 26`
+~~~
+
+The script will only check for episodes starting episode 26, and will convert
+it to:
+
+`Attack on Titan/Season 02/Attack on Titan - S02E01.mkv`
+
+If you add __season_end__:
+
+`"season_end": 37`
+
+Then the script will ignore episodes 38 and forward, as they are not of this
+particular season.
--- getnyaa.py
+++ getnyaa.py
@@ -9,7 +9,7 @@
 # anime library once completed. Does the cleanup of finished
 # torrents too.
 #
-# 2020 Rafael Fernandes - Public Domain
+# 2020-2022 Rafael Fernandes - Public Domain
 #
 
 import xml.etree.ElementTree as XML
@@ -27,154 +27,181 @@ print("Starting getnyaa:")
 global CONFIG
 with open("config.json") as f:
     CONFIG = json.load(f)
+    TRANSMISSION_LOGIN = f'{CONFIG["user"]}:{CONFIG["password"]}'
+    LIBRARY_DIR = CONFIG["library_dir"]
 
-def episodeFolder(anime_name,season):
-    return f'{CONFIG["library_dir"]}/{anime_name}/Season {season:02}/'
 
-def episodeFilename(anime_name,season,episode):
-    return f'{anime_name} - S{season:02}E{episode:02}'
+def make_episode_path(anime_name, episode, season=None):
+    if season is None:
+        ep_filename = f'{anime_name} - {episode:02}'
+    else:
+        ep_filename = f'Season {season:02}/{anime_name} - S{season:02}E{episode:02}'
+    return Path(f'{LIBRARY_DIR}/{anime_name}/{ep_filename}')
 
-def getTransmissionLogin():
-    return f'{CONFIG["user"]}:{CONFIG["password"]}'
 
-def runTransmission(args,capture=False):
-    targs = ['transmission-remote','-n',getTransmissionLogin()] + args
-    return subprocess.run(targs,capture_output=capture,text=True)
+def has_episode(episode_path):
+    anime_path = episode_path.parent
+    if (not anime_path.exists()):
+        return False
+    for f in anime_path.glob(episode_path.name + ".*"):
+        return f.stem == episode_path.name
+    return False
 
-def getOrganizeFilename(torrent_hash):
+
+def transmission_cmd(args, capture=False):
+    targs = ['transmission-remote', '-n', TRANSMISSION_LOGIN] + args
+    return subprocess.run(targs, capture_output=capture, text=True)
+
+
+def get_organize_filename(torrent_hash):
     return f'{CONFIG["download_dir"]}/{torrent_hash}.getnyaa'
 
-# 
+#
 # Check for missing episodes from Nyaa.si RSS feeds
 # and add them to transmission
 #
 
-def addEpisodeTorrent(url):
-    runTransmission(['-a',url])
 
-def isMissingEpisode(season_folder,filename):
-    folder = Path(season_folder)
-    if (not folder.exists()):
-        return True
-    for f in folder.iterdir():
-        if (str.upper(f.stem) == str.upper(filename)):
-            return False
-    return True
+def add_torrent(url):
+    transmission_cmd(['-a', url])
 
-def addOrganizeFile(thash,target_folder,target_name):
-    org_filename = getOrganizeFilename(thash)
-    print("Adding organize file",org_filename)
-    org_info = {
-        "season_dir": target_folder,
-        "episode": target_name
-    }
+
+def add_organize_file(thash, destination):
+    org_filename = get_organize_filename(thash)
+    print("Adding organize file", org_filename)
     with Path(org_filename) as p:
         p.parent.mkdir(parents=True, exist_ok=True)
-        p.write_text(json.dumps(org_info))
+        print(str(destination))
+        p.write_text(str(destination))
 
-def isWantedEpisode(title, url, thash, anime_list):
+
+def check_episode(title, url, thash, anime_list):
     for anime in anime_list:
-        if not re.search(anime["search_re"],title):
-            continue # test next anime in anime_list
+        match = re.search(anime["search_re"], title)
+        if not match:
+            continue  # test next anime in anime_list
         print(f'Found match for {title}')
         try:
-            item_episode = int(re.search(anime["episode_re"],title).group(1))
+            episode = int(match.group(1))
+            season_start = anime.get("season_start")
+            if season_start is not None:
+                # convert episode numbering
+                if "season" in anime:
+                    # from overall to seasonal numbering
+                    episode = episode - season_start + 1
+                    if episode <= 0:
+                        return # this episode is from previous season, ignore
+                    season_end = anime.get("season_end")
+                    if (season_end is not None) and (episode > season_end):
+                        return # this episode is from next season, ignore
+                else:
+                    # from seasonal to overall
+                    episode = episode - 1 + season_start
         except:
-            print("Could not find episode number from torrent title, check 'episode_re' on config.json")
+            print("Could not find episode number from torrent title, check 'search_re' on config.json")
             return
-        season_folder = episodeFolder(anime["name"],anime["season"])
-        episode_file = episodeFilename(anime["name"],anime["season"],item_episode)
-        print("Checking if",episode_file,"is missing...")
-        if (isMissingEpisode(season_folder,episode_file)):
-            print(episode_file,"missing, adding torrent to transmission.")
-            addEpisodeTorrent(url)
-            addOrganizeFile(thash,season_folder,episode_file)
+        episode_path = make_episode_path(anime["name"], episode, anime.get("season"))
+        print("Checking if", episode_path.name, "is missing...")
+        if (not has_episode(episode_path)):
+            print(episode_path.name, "missing, adding torrent to transmission.")
+            add_torrent(url)
+            add_organize_file(thash, episode_path)
         else:
-            print(episode_file,"already in library, skipping.")
-        return # go to next item in RSS
+            print(episode_path.name, "already in library, skipping.")
+        return  # go to next item in RSS
 
-def findEpisodes(user,anime_list):
+
+def check_rss_episodes(user, anime_list):
     from urllib.request import urlopen
-    ns = { "nyaa" : "https://nyaa.si/xmlns/nyaa" } 
+    ns = {"nyaa": "https://nyaa.si/xmlns/nyaa"}
     try:
         rss = urlopen(f"https://nyaa.si/?page=rss&user={user}")
         src_rss = XML.fromstring(rss.read())
     except:
-        print("Could not load",user,"RSS!")
+        print("Could not load", user, "RSS!")
         return
     for item in src_rss.findall("channel/item"):
-        title =  item.find("title").text
+        title = item.find("title").text
         url = item.find("link").text
-        thash = item.find("nyaa:infoHash",ns).text
-        isWantedEpisode(title,url,thash,anime_list)
+        thash = item.find("nyaa:infoHash", ns).text
+        check_episode(title, url, thash, anime_list)
 
-def checkNewEpisodes(src_list):
+
+def search_new_episodes(src_list):
     for src in src_list:
-        print("Processing",src["user"])
-        findEpisodes(src["user"],src["animes"])
+        print("\nProcessing", src["user"])
+        check_rss_episodes(src["user"], src["animes"])
 
-print("\nChecking for new episodes:")
-checkNewEpisodes(CONFIG["sources"])
+
+print("Searching for new episodes:")
+search_new_episodes(CONFIG["sources"])
 
 #
 # Check if download finished, and copy to destination anime folder
 #
 
-def removedTorrent(thash):
-    r = runTransmission(['-t',thash])
+
+def is_torrent_removed(thash):
+    r = transmission_cmd(['-t', thash])
     return r.returncode != 0
 
-def getDownloadStatus(thash):
-    r = runTransmission(['-t',thash,'-i'],capture=True)
+
+def get_download_status(thash):
+    r = transmission_cmd(['-t', thash, '-i'], capture=True)
     downloaded = False
     finished = False
     for l in r.stdout.splitlines():
-        match = re.search("State: (\\w+)",l)
+        match = re.search(r'State: (\w+)', l)
         if (match and match.group(1) == "Finished"):
             finished = True
-        match = re.search('Percent Done: (.*)%',l)
+        match = re.search('Percent Done: (.*)%', l)
         if (match and match.group(1) == "100"):
             downloaded = True
-    return (downloaded,finished)
+    return (downloaded, finished)
 
-def getDownloadFilename(thash):
-    r = runTransmission(['-t',thash,'-f'],capture=True)
+
+def get_download_filename(thash):
+    r = transmission_cmd(['-t', thash, '-f'], capture=True)
     lines = r.stdout.splitlines()
     t_header = lines[1]
     t_file = lines[2]
-    m = re.search("Name",t_header)
+    m = re.search("Name", t_header)
     f_start = m.span()[0]
     filename = t_file[f_start:]
     return filename
 
-def copyToLibrary(thash,info):
+
+def copy_to_library(thash, dst_path):
     from shutil import copyfile
-    src_path = Path(CONFIG["download_dir"]) / getDownloadFilename(thash)
-    dst_file = info["episode"] + src_path.suffix
-    dst_path = Path(info["season_dir"]) / dst_file
-    if (dst_path.exists()): return # dont overwrite existing file
-    print("Copying new episode",src_path,"to",dst_path,"...")
-    dst_path.parent.mkdir(parents=True,exist_ok=True)
-    copyfile(src_path,dst_path)
+    src_file = Path(CONFIG["download_dir"]) / get_download_filename(thash)
+    dst_file = Path(str(dst_path) + src_file.suffix)
+    if (dst_file.exists()):
+        return  # dont overwrite existing file
+    print("Copying new episode", src_file, "to", dst_file, "...")
+    dst_file.parent.mkdir(parents=True, exist_ok=True)
+    copyfile(src_file, dst_file)
 
-def cleanTorrent(thash,info):
-    print("Cleaning torrent of",info["episode"],"...")
-    runTransmission(['-t',thash,'--remove-and-delete'])
 
-def checkDownloads():
+def clean_torrent(thash, path):
+    print("Cleaning torrent of", path.name, "...")
+    transmission_cmd(['-t', thash, '--remove-and-delete'])
+
+
+def check_downloads():
     for f in Path(CONFIG["download_dir"]).glob("*.getnyaa"):
         thash = f.stem
-        info = json.loads(f.read_text())
-        print("Checking torrent",thash,"of episode",info["episode"],"...")
-        if (removedTorrent(thash)):
+        dst_path = Path(f.read_text())
+        print("Checking torrent", thash, "of episode", dst_path.name, "...")
+        if (is_torrent_removed(thash)):
             f.unlink()
             continue
-        (downloaded,finished) = getDownloadStatus(thash)
+        (downloaded, finished) = get_download_status(thash)
         if (downloaded):
-            copyToLibrary(thash,info)
+            copy_to_library(thash, dst_path)
         if (finished):
-            cleanTorrent(thash,info)
+            clean_torrent(thash, dst_path)
             f.unlink()
 
+
 print("\nChecking downloads:")
-checkDownloads()
+check_downloads()
